2fdada93b8b8a01238f6b45e383413a40ccfc4ed
Danny Zhang <xin.zhang@niu.com>
2024-03-27 22:50:33 +0800

    feat:init config && header,footer

---
+++ components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="mx-auto w-4/6">
+        <nuxt-link to="/">
+            <NuxtImg src="/favicon.ico" loading="lazy" />
+        </nuxt-link>
+        <UButton label="Book" />
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui',
+    // '@nuxtjs/eslint-module', // for Coding
+    '@nuxt/image', // for Optimization
+    "nuxt-security", // for Security
+    "nuxt-csurf", // for CSRF
+    '@nuxtjs/seo', // for SEO
+    '@vite-pwa/nuxt', // for PWA
+    ['nuxt-gtag', { gtag: { id: 'G-XXXXXXXXXX'} }] // for Google Tags
+  ],
 })
